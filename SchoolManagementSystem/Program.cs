10e97d33d47b6ecda5b132da6a5b27809f68ed2c
Muhammad Hashir <hashiraptech50@gmail.com>
2024-06-02 17:56:09 +0500

    Add Auth service

--- SchoolManagementSystem/Program.cs
+++ SchoolManagementSystem/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 
 //builder.Services.Configure<FormOptions>(options =>
